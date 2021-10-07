cd7edbc3cd01ac622fda5448f6d1cf109416f5e6
EasyBAMM <huhjb1020@naver.com>
2021-10-07 09:38:21 +0900

    chore : 주문목록 페이징 처리 진행중

--- team1_project1/src/main/java/com/mycompany/webapp/controller/OrderController.java
+++ team1_project1/src/main/java/com/mycompany/webapp/controller/OrderController.java
@@ -11,10 +11,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 import com.mycompany.webapp.dto.MemberDetails;
+import com.mycompany.webapp.dto.Pager;
 import com.mycompany.webapp.dto.ProductOrder;
 import com.mycompany.webapp.service.OrderService;
 import com.mycompany.webapp.service.OrderService.OrderResult;
@@ -50,6 +51,29 @@ public class OrderController {
 		return json.toString();
 	}
 
+	// 주문목록페이지 - 목록리스트 페이징 적용(JSON)(진행중)
+	@GetMapping(value = "/getorderlistbypage", produces = "Application/json; charset=UTF-8;")
+	@ResponseBody
+	public String getOrderListByPage(Authentication authentication,
+			@RequestParam(name = "pageNo", defaultValue = "1") int pageNo) {
+		logger.info("실행");
+		JSONObject json = new JSONObject();
+		if (authentication != null) {
+			// 로그인한 유저 정보의 넘버로 데이터 얻기
+			MemberDetails memberDetails = (MemberDetails) authentication.getPrincipal();
+			int mno = memberDetails.getMno();
+
+			// 주문목록 전체 갯수
+			int totalRows = orderService.getOrderListCount(mno);
+			Pager pager = new Pager(5, 5, totalRows, pageNo);
+
+			json.put("result", orderService.getOrderListByPage(mno, pager));
+		} else {
+			json.put("result", "fail");
+		}
+		return json.toString();
+	}
+
 	// 주문상세페이지
 	@RequestMapping("/orderdetail")
 	public String orderDetail() {
@@ -72,31 +96,30 @@ public class OrderController {
 		}
 		return json.toString();
 	}
-	
+
 	@PostMapping(value = "/newOrder", produces = "Application/json; charset=UTF-8;")
 	public String newOrder(ProductOrder order, String plist, Model model) {
 		int mno = 1;
 		int mpoint = 500;
 		order.setMno(mno);
-		OrderResult result = orderService.newOrder(order,plist, mpoint);
+		OrderResult result = orderService.newOrder(order, plist, mpoint);
 
-		if(result.equals(OrderResult.SUCCESS)) {
-			return "redirect:/orderdetail?code="+order.getPorderno();
-		}else if (result.equals(OrderResult.ENOUGH_MPOINT)) {
+		if (result.equals(OrderResult.SUCCESS)) {
+			return "redirect:/orderdetail?code=" + order.getPorderno();
+		} else if (result.equals(OrderResult.ENOUGH_MPOINT)) {
 			model.addAttribute("errorTitle", "사용 가능한 마일리지가 부족합니다.");
 			model.addAttribute("errorContent", "정상적인 방식으로 마일리지를 적용해주세요.");
 			return "error/custom";
-		}else if(result.equals(OrderResult.NOT_VALID)) {
+		} else if (result.equals(OrderResult.NOT_VALID)) {
 			model.addAttribute("errorTitle", "잘못된 상품 결제입니다.");
 			model.addAttribute("errorContent", "정상적인 방식으로 결제를 진행해주세요.");
 			return "error/custom";
-		}else {
+		} else {
 			model.addAttribute("errorTitle", "알 수 없는 서버 오류입니다.");
 			model.addAttribute("errorContent", "잠시후 다시 실행해주세요.");
 			return "error/custom";
 		}
 
 	}
-	
 
 }
--- team1_project1/src/main/java/com/mycompany/webapp/dao/OrderDAO.java
+++ team1_project1/src/main/java/com/mycompany/webapp/dao/OrderDAO.java
@@ -3,16 +3,26 @@ package com.mycompany.webapp.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.mycompany.webapp.dto.OrderDetail;
 import com.mycompany.webapp.dto.OrderList;
+import com.mycompany.webapp.dto.Pager;
 import com.mycompany.webapp.dto.ProductOrder;
 
 @Mapper
 public interface OrderDAO {
-	public List<OrderList> getOrderList(int mNo);
+	// 주문 목록 가져오기(유저번호)
+	public List<OrderList> getOrderList(int mno);
 
-	public List<OrderDetail> getOrderDetail(int mNo, int pOrderNo);
-	
+	// 주문 목록 가져오기(유저번호) 페이징
+	public List<OrderList> getOrderListByPage(@Param("mno") int mno, @Param("pager") Pager pager);
+
+	// 주문 목록 갯수 가져오기(유저번호)
+	public int getOrderListCount(int mno);
+
+	// 주문 상세내용 가져오기(유저번호, 주문번호)
+	public List<OrderDetail> getOrderDetail(int mno, int porderno);
+
 	public int createOrder(ProductOrder po);
 }
